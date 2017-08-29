feded33409395047d9a87707831d572dc76fccbc
高橋岳宏 <taka.mc.0509@gmail.com>
2017-08-29 22:43:03 +0900

    receiveカラムを削除し、room_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829122011) do
+ActiveRecord::Schema.define(version: 20170829133109) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content", null: false
     t.integer "user_id", null: false
-    t.integer "receiver", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "room_id"
   end
 
   create_table "users", force: :cascade do |t|
