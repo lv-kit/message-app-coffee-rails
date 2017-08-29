e0a7ba2f940d7e04ab33426c2f87e826085d2735
高橋岳宏 <taka.mc.0509@gmail.com>
2017-08-29 22:52:08 +0900

    receiverではなく、room_idに変更

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
   def create
-    @message = Message.create(params.require(:message).permit(:content, :user_id, :receiver).merge(user_id: current_user.id))
-    redirect_back(fallback_location: user_path(@message.receiver))
+    @message = Message.create(params.require(:message).permit(:content, :user_id, :room_id).merge(user_id: current_user.id))
+    redirect_back(fallback_location: room_path(@message.room_id))
   end
 end
