9816fed4c1a572b815c59dbec2869263c344bf8d
高橋岳宏 <taka.mc.0509@gmail.com>
2017-08-29 20:43:54 +0900

    ユーザー一覧画面を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :show, :edit, :update]
 
   def index
+    @users = User.all.order(created_at: 'desc')
   end
 
   def show
