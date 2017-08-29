383e65391fb0823aa9edde4fb7ccb268fdcd1c11
高橋岳宏 <taka.mc.0509@gmail.com>
2017-08-29 20:40:39 +0900

    user詳細ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
 
+  before_action :authenticate_user!, only: [:index, :show, :edit, :update]
+
   def index
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
