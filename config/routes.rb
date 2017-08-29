2d1146d8f6476bc2b6aafcc2e245a7078228301d
高橋岳宏 <taka.mc.0509@gmail.com>
2017-08-29 20:33:37 +0900

    user関連のファイルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :welcome, only: [:index]
+  resources :users, only: [:index, :show, :edit, :update]
   root "welcome#index"
 end
