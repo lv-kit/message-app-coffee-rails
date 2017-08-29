4e51f2c96f6dfad6da2f0aa7dec03cbc431744c1
高橋岳宏 <taka.mc.0509@gmail.com>
2017-08-29 23:50:43 +0900

    チャットルームの詳細ページ

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,9 +3,15 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :show, :index, :edit, :update]
 
   def create
+    @room = Room.create
+    redirect_to room_path(@room.id)
+    flash[:notice] = "チャットを作成しました。"
   end
 
   def show
+    @room = Room.find(params[:id])
+    @messages = @room.messages
+    @message = Message.new
   end
 
   def index
@@ -15,5 +21,11 @@ class RoomsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, :explain)
   end
 end
