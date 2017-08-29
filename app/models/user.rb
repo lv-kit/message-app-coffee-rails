918a5e9abfb56e1c4ac88d57f7da1942009d3581
高橋岳宏 <taka.mc.0509@gmail.com>
2017-08-29 22:27:51 +0900

    テーブル毎のヒモ付

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   validates :family_name, presence: true, length: { minimum: 1 }
   validates :first_name, presence: true, length: { minimum: 1 }
   validates :self_introduction, length: { minimum: 2 }
+
+  has_many :messages
 end
