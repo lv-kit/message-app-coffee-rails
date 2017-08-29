1dda46308c67f394d0c028b7229975415629d1a4
高橋岳宏 <taka.mc.0509@gmail.com>
2017-08-29 23:09:24 +0900

    ユーザー画面の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
-  validates :family_name, presence: true, length: { minimum: 1 }
-  validates :first_name, presence: true, length: { minimum: 1 }
-  validates :self_introduction, length: { minimum: 2 }
 
   has_many :messages
 end
